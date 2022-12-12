e1e082648e942980fb950a9d7ca44ebd314072cc
ThisNarciss <tyomaxxx000@gmail.com>
2022-12-12 15:35:02 +0200

    update hw 10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+const debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.querySelector('input#search-box');
+const listRef = document.querySelector('.country-list');
+const countryInfoRef = document.querySelector('.country-info');
+
+inputRef.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function onInputSearch(evt) {
+  const inputVal = evt.target.value.trim();
+  console.log(fetchCountries(inputVal));
+  fetchCountries(inputVal)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (countries.length >= 2 && countries.length <= 10) {
+        renderCountries(countries);
+      }
+    })
+    .catch(
+      error =>
+        Notiflix.Notify.failure('Oops, there is no country with that name')
+      // console.log(error)
+    );
+}
+
+function renderCountries(countries) {
+  const countriesMarkUp = countries
+    .map(country => {
+      return `<li>${country.name.official}</li>`;
+    })
+    .join('');
+  listRef.innerHTML = countriesMarkUp;
+}
+// Ukraine
